f24aae22226494aa48b3b126d52ed572c11a0fca
sammy17 <chathura.abeyrathne.lk@gmail.com>
2018-01-12 23:50:34 +0530

    all done

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sys
 import cv2
-import cv2.cv as cv
 import numpy as np
-from PIL import Image
-from progress import *
+import image
 import time
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 
 SCALE = 0.5
 NOISE_CUTOFF = 5
@@ -15,41 +15,41 @@ start = time.time()
 def count_diff(img1, img2):
     small1 = cv2.resize(img1, (0,0), fx=SCALE, fy=SCALE)
     small2 = cv2.resize(img2, (0,0), fx=SCALE, fy=SCALE)
-    #cv2.imshow('frame', small2)
-    #cv2.waitKey(1)
     diff = cv2.absdiff(small1, small2)
     diff = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
     frame_delta1 = cv2.threshold(diff, NOISE_CUTOFF, 255, 3)[1]
-    frame_delta1_color = cv2.cvtColor(frame_delta1, cv2.COLOR_GRAY2RGB)
+    #frame_delta1_color = cv2.cvtColor(frame_delta1, cv2.COLOR_GRAY2RGB)
     delta_count1 = cv2.countNonZero(frame_delta1)
 
     return delta_count1
 
 filename = sys.argv[1]
+treshold = float(sys.argv[2])
+if not (treshold > 0 and treshold <100):
+    print("Invalid threshold value -> threshold must be greater than 0 and less than 100\n")
+    sys.exit()
 video = cv2.VideoCapture(filename)
 
-nframes = (int)(video.get(cv.CV_CAP_PROP_FRAME_COUNT))
-width = (int)(video.get(cv.CV_CAP_PROP_FRAME_WIDTH))
-height = (int)(video.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
-fps= (int)(video.get(cv.CV_CAP_PROP_FPS))
+nframes = (int)(video.get(cv2.CAP_PROP_FRAME_COUNT))
+width = (int)(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = (int)(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps= (int)(video.get(cv2.CAP_PROP_FPS))
 
 frame_count = 0
 
-print "[i] Frame size: ", width, height
-print "[i] Total frames:", nframes
-print "[i] Fps:", fps
+print( "[i] Frame size: ", width, height)
+print("[i] Total frames:", nframes)
+print("[i] Fps:", fps)
+
+treshold = treshold * width * height/100
 
 fff = open(filename+".csv", 'w')
 
 flag, frame0 = video.read()
-treshold = int(sys.argv[2])
+
 strikes = 0
 
-#p0 = progress.AnimatedProgressBar(end=nframes, width=80)
-for f in xrange(nframes-1):
-    #p0 + 1
-    #p0.show_progress()
-
+for f in range(nframes-1):
     flag, frame1 = video.read()
     diff1  = count_diff(frame0, frame1)
     name = filename+"_%06d.jpg" % f
@@ -58,17 +58,11 @@ for f in xrange(nframes-1):
         cv2.imwrite(name, frame1)
         strikes = strikes + 1
 
-        #small = cv2.resize(frame1, (0,0), fx=SCALE, fy=SCALE)
-        #cv2.imshow('frame', small)
-        #cv2.waitKey(1)
-
     text = str(f)+', '+str(diff1)
-    #print text
     fff.write(text  + '\n')
     fff.flush()
     frame0 = frame1
 
 fff.close()
-#print
-print '[i] Strikes: ', strikes
-print '[i] elapsed time:', time.time() - start
+print( '[i] Strikes: ', strikes)
+print( '[i] elapsed time:', time.time() - start)
